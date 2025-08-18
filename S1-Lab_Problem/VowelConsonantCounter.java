e05025f0e9cb0202199fddaa9e4e7dcbdf331ab5
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-08-18 08:26:40 +0530

    Add files via upload

---
+++ S1-Lab_Problem/VowelConsonantCounter.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class VowelConsonantCounter 
+{
+
+    public static String checkCharType(char ch) {
+        
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32); 
+        }
+
+        if (ch >= 'a' && ch <= 'z') 
+		{
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        } else {
+            return "Not a Letter";
+        }
+    }
+
+    
+    public static int[] countVowelsAndConsonants(String str) {
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String result = checkCharType(ch);
+
+            if (result.equals("Vowel")) {
+                vowels++;
+            } else if (result.equals("Consonant")) {
+                consonants++;
+            }
+        }
+
+        return new int[]{vowels, consonants};
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        int[] result = countVowelsAndConsonants(input);
+
+        System.out.println("Number of Vowels: " + result[0]);
+        System.out.println("Number of Consonants: " + result[1]);
+
+        sc.close();
+    }
+}
