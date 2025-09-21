23597bf17d90fb39f0d42623bfdd5f3f61dcae21
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-21 17:37:51 +0530

    Add files via upload

---
+++ Inheritance/Lab_Problems/Color.java
@@ -0,0 +1,41 @@
+class Color {
+    protected String name;
+
+    public Color(String name) {
+        this.name = name;
+        System.out.println("Color constructor: " + name);
+    }
+}
+
+class PrimaryColor extends Color {
+    protected int intensity;
+
+    public PrimaryColor(String name, int intensity) {
+        super(name);
+        this.intensity = intensity;
+        System.out.println("PrimaryColor constructor: intensity " + intensity);
+    }
+}
+
+class RedColor extends PrimaryColor {
+    private String shade;
+
+    public RedColor(String name, int intensity, String shade) {
+        super(name, intensity);
+        this.shade = shade;
+        System.out.println("RedColor constructor: shade " + shade);
+    }
+
+    public void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Intensity: " + intensity);
+        System.out.println("Shade: " + shade);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        RedColor red = new RedColor("Red", 90, "Crimson");
+        red.showDetails();
+    }
+}
