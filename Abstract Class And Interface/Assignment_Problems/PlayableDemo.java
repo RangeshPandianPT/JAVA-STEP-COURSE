fcaef8fe46398793c8e0e3bd5f1711c07439722a
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-10-05 09:22:34 +0530

    Add files via upload

---
+++ Abstract Class And Interface/Assignment_Problems/PlayableDemo.java
@@ -0,0 +1,28 @@
+interface Playable {
+    void play();
+    void pause();
+}
+
+class MusicPlayer implements Playable {
+    public void play() { System.out.println("Playing music..."); }
+    public void pause() { System.out.println("Music paused."); }
+}
+
+class VideoPlayer implements Playable {
+    public void play() { System.out.println("Playing video..."); }
+    public void pause() { System.out.println("Video paused."); }
+}
+
+public class PlayableDemo {
+    public static void main(String[] args) {
+        Playable ref;
+
+        ref = new MusicPlayer();
+        ref.play();
+        ref.pause();
+
+        ref = new VideoPlayer();
+        ref.play();
+        ref.pause();
+    }
+}
