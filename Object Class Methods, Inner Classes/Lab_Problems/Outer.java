5b87fb64e19f58f82571748498ec5e9fc8dfab15
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-10-17 10:39:14 +0530

    Add files via upload

---
+++ Object Class Methods, Inner Classes/Lab_Problems/Outer.java
@@ -0,0 +1,15 @@
+public class Outer {
+    private String message = "Hello from the Inner Class!";
+
+    class Inner {
+        void display() {
+            System.out.println(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        Outer outerObj = new Outer();
+        Outer.Inner innerObj = outerObj.new Inner();
+        innerObj.display();
+    }
+}
